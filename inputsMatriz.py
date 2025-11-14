818f88288258098aefd6f93b99383b49a3ffea75
DianaHang <dianavegat99@gmail.com>
2025-11-13 23:02:10 -0600

    Create matrix inputs validation file

--- inputsMatriz.py
+++ inputsMatriz.py
@@ -8,16 +8,16 @@ import numpy as np
 #Input para num de filas y columnas
 def pedirFilCol():
     while True:
-        fStr = input("Número de filas: ")
-        cStr = input("Número de columnas: ")
+        fStr = input("Número de filas de la matriz A: ")
+        cStr = input("Número de columnas de la matriz A: ")
         
         #Validar si se recibe una entrada numérica
         try:
           filas = int(fStr)
           cols = int(cStr)
           
-          if filas != cols: print("El pivoteo requiere de una matriz cuadrada.\n")
-          elif filas < 2: print("El pivoteo sirve a partir de sistemas 2x2.\n")
+          if filas != cols: print("El método requiere de una matriz cuadrada.\n")
+          elif filas < 2: print("El método sirve a partir de sistemas 2x2.\n")
           else: return filas, cols
           
         except ValueError:
@@ -71,11 +71,40 @@ def llenarVectorB(filas):
 def mostrarDatos(A, b):
     print("\nMatriz de coeficientes: \nA= ")
     print(A)
-    print("\nVector de térmminos independientes: \nb= ")
+    print("\nVector de términos independientes: \nb= ")
     print(b)
+    
+def diagonalDominante(A, b):
+    n = len(A)
+    #Almacenar los inputs 
+    A = A.copy()
+    b = b.copy()
+
+    # Buscar la fila donde el elemento[k][i] sea dominante
+    for i in range(n):
+        maxFila = i
+        for k in range(i + 1, n):
+            if abs(A[k][i]) > abs(A[maxFila][i]):
+                maxFila = k
+        
+        # Intercambiar filas
+        if maxFila != i:
+            A[[i, maxFila]] = A[[maxFila, i]]
+            b[[i, maxFila]] = b[[maxFila, i]]
+
+        # Comprobar diagonal dominante: sum -> suma de elementos de estructura iterable
+        sumaDiag = sum(abs(A[i,j]) for j in range(n) 
+                       if j != i)
+        if abs(A[i,i]) < sumaDiag:
+            return None, None
+
+    return A, b
+    print("\nMatriz diagonal dominante: ")
+    print(A)
+
             
 def main():
-    print("**SEL en Forma matricial***")
+    print("**SEL EN FORMA MATRICIAL***")
     filas, cols = pedirFilCol()
     A = llenarMatriz(filas, cols)
     bCol = llenarVectorB(filas)
