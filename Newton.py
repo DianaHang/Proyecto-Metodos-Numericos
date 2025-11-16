f0f4c0334b00e22c617e9de32c717811d24f25c1
DianaHang <dianavegat99@gmail.com>
2025-11-16 01:02:21 -0600

    Separate mp3 files into Assets directory

--- Newton.py
+++ Newton.py
@@ -1,6 +1,6 @@
 '''
     Este programa implementa la solución que satisface 
-    las ecuaciones 3x - (x+2)^2 * e^(-x) = 0 
+    la ecuación 3x - (x+2)^2 * e^(-x) = 0 
     mediante el uso del método de Newton 
 '''
 import math
@@ -8,24 +8,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Función muestra
-def f1(x):
+def funcion1(x):
     return (3*x) - pow(x+2, 2) * math.exp(-x)
-
-def f2(x):
-    return math.cos(x) + 2 * math.sin(x) + pow(x, 2)
-
-def f3(x):
-    return math.log(abs(x-1)) + math.cos(x-1)
 
 def derivf1(x):
     return 3 - 2*(x+2)*math.exp(-x) + (x+2)**2 * math.exp(-x)
-  
-def derivf2(x):
-    return - math.sin(x) + 2 * math.cos(x) + 2*x
-
-def derivf3(x):
-    return 1 / (x - 1) - math.sin(x - 1)
-  
+    
 #Función para validar inputs numéricos
 def leerInput():
     while True:
@@ -49,7 +37,7 @@ def metodoNewton(f, derivada, x0, n = 10):
     
     #Realizar iteraciones con método de Newton
     for i in range(n):
-      f = f1(xn)
+      f = funcion1(xn)
       deriv = derivada(xn)
       
       #Validar no división por cero
@@ -78,27 +66,22 @@ def metodoNewton(f, derivada, x0, n = 10):
     return df, xList[-1]
 
 #Main
-def main():
-    print("***MÉTODO DE NEWTON***\n")
-    x0 = leerInput()
-    dfResul, raiz = metodoNewton(f1, derivf1, x0, n = 10)
-    
-    #Imprimir tabla con Pandas
-    pd.set_option('display.float_format', '{:.4f}'.format)
-    print("\nResultados del método de Newton:\n")
-    print(dfResul.to_string(index=False))
-    
-    print(f"\nLa solución aproximada para la función f(x) = 3x - (x+2)^2 * e^(-x) es: \n x ≈ {raiz:.6f}")
-    print(f"\n f(x) ≈ {f1(raiz):.6f}")
-    
-    # Gráfica para mostrar convergencia del método
-    plt.figure(figsize=(8,5))
-    plt.plot(dfResul["n"], dfResul["xn"], marker='*', linestyle='-', color='green')
-    plt.title("Convergencia del Método de Newton", fontsize=14)
-    plt.xlabel("Número de iteraciones", fontsize=12)
-    plt.ylabel("Aproximación xn", fontsize=12)
-    plt.grid(True, linestyle='--', alpha=0.6)
-    plt.show()
-    
-if __name__ == "__main__":
-    main()
+print("***MÉTODO DE NEWTON***\n")
+x0 = leerInput()
+dfResul, raiz = metodoNewton(funcion1, derivf1, x0, n = 10)
+#Imprimir tabla con Pandas
+pd.set_option('display.float_format', '{:.4f}'.format)
+print("\nResultados del método de Newton-Raphson:\n")
+print(dfResul.to_string(index=False))
+
+print(f"\nLa solución aproximada para la función f(x) = 3x - (x+2)^2 * e^(-x) es: \n x ≈ {raiz:.4f}")
+
+# GRÁFICA PARA CONVERGENCIA DEL MÉTODO -------------------
+
+plt.figure(figsize=(8,5))
+plt.plot(dfResul["n"], dfResul["xn"], marker='o', linestyle='-', color='teal')
+plt.title("Convergencia del Método de Newton", fontsize=14)
+plt.xlabel("Número de iteraciones", fontsize=12)
+plt.ylabel("xn", fontsize=12)
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.show()
